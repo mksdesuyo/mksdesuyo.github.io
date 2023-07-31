a524be6cb3aacf33b2cbbc3b3192ca91835d27a6
mksdesuyo <rizqimks@gmail.com>
2023-07-31 16:28:33 +0700

    feat(navbar): add brand name & working theme-switch button

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,24 @@
-console.log("Hey!");
+const themeSwitcherButton = document.querySelector("#themeSwitcher");
+const theme = document.body;
+let initialText = `</span><i class="fa-regular fa-moon"></i> Dark<span>`;
+themeSwitcherButton.innerHTML = initialText;
+
+themeSwitcherButton.addEventListener('click', () => {
+    changedText = `</span><i class="fa-regular fa-sun"></i> Light<span>`;
+
+    if(theme.classList == 'light') {
+        theme.classList.replace('light', 'dark');
+        themeSwitcherButton.innerHTML = changedText;
+        localStorage.setItem('theme', 'dark');
+    } else {
+        theme.classList.replace('dark', 'light');
+        themeSwitcherButton.innerHTML = initialText;
+        localStorage.setItem('theme', 'light');
+    }
+})
+
+if(localStorage.getItem('theme') == 'light') {
+    theme.classList = 'light';
+} else {
+    theme.classList = 'dark';
+}
